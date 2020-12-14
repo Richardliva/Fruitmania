a7378221962c69bc545174cd095cddd23ad94393
Richardliva <63636715+Richardliva@users.noreply.github.com>
2020-12-14 02:25:02 -0800

    Add files via upload

---
+++ BackAndroid.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class BackAndroid : MonoBehaviour
+{
+public string ParentLeveleName;
+
+void Update()
+{
+if (Input.GetKeyDown(KeyCode.Escape))
+{
+if (ParentLeveleName != "Quit")
+Application.LoadLevel(ParentLeveleName);
+else
+Application.Quit();
+}
+}
+}
